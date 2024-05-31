1a6160011f343c50f516c22d11d2ed9326c2c08e
Micah Woodard <micah.woodard@alleninstitute.org>
2024-05-31 11:19:43 -0700

    feat: modifies filter wheel to work when filter objects are enabled

--- examples/filter_wheel_example.py
+++ examples/filter_wheel_example.py
@@ -2,6 +2,22 @@ from view.widgets.device_widgets.filter_wheel_widget import FilterWheelWidget
 from qtpy.QtWidgets import QApplication
 import sys
 from voxel.devices.filterwheel.simulated import FilterWheel
+from voxel.devices.filter.simulated import Filter
+from time import sleep
+import threading
+
+
+def move_filter():
+    widget.filter = 'BP561'
+    sleep(3)
+    widget.filter = 'MB405/488/561/638'
+    sleep(3)
+    filter_wheel.filter = 'BP405'
+    sleep(3)
+    filter_wheel.filter = 'LP638'
+    sleep(3)
+    BP405_filter.enable()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
@@ -12,7 +28,17 @@ if __name__ == "__main__":
                                 'MB405/488/561/638': 4,
                                 'Empty1': 5,
                                 'Empty2': 6,})
+
+    BP405_filter = Filter(filter_wheel, 'BP405')
+    BP561_filter = Filter(filter_wheel, 'BP561')
+    LP638_filter = Filter(filter_wheel, 'LP638')
+    BP488_filter = Filter(filter_wheel, 'BP488')
+
+
     widget = FilterWheelWidget(filter_wheel)
     widget.show()
-    #filterwheel.set_index('561')
+
+    t1 = threading.Thread(target=move_filter)
+    t1.start()
+
     sys.exit(app.exec_())
