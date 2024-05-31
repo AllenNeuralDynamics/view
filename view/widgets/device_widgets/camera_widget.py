b92a7bff42cf429323dd5ff9d86294a7763441fb
Micah Woodard <micah.woodard@alleninstitute.org>
2024-05-31 16:24:45 -0700

    feat: disable widgets if they don't have a setter. Update base class as well

--- view/widgets/device_widgets/camera_widget.py
+++ view/widgets/device_widgets/camera_widget.py
@@ -29,19 +29,33 @@ class CameraWidget(BaseDeviceWidget):
             pixel_widgets = create_widget('VH',
                                *self.property_widgets.get('binning', _).findChildren(QWidget, options=direct),
                                *self.property_widgets.get('pixel_type', _).findChildren(QWidget, options=direct))
+            # check if properties have setters and if not, disable widgets. Have to do it inside pixel widget
+            for i, prop in enumerate(['binning', 'pixel_type']):
+                attr = getattr(type(camera), prop)
+                if getattr(attr, 'fset', None) is None:
+                    pixel_widgets.children()[i+1].setEnabled(False)
 
             # reformat timing widgets
             timing_widgets = create_widget('VH',
                                *self.property_widgets.get('exposure_time_ms', _).findChildren(QWidget, options=direct),
                                *self.property_widgets.get('frame_time_ms', _).findChildren(QWidget, options=direct),
                                *self.property_widgets.get('line_interval_us', _).findChildren(QWidget, options=direct))
+            # check if properties have setters and if not, disable widgets
+            for i, prop in enumerate(['exposure_time_ms', 'frame_time_ms', 'line_interval_us']):
+                attr = getattr(type(camera), prop, False)
+                if getattr(attr, 'fset', None) is None:
+                    timing_widgets.children()[i+1].setEnabled(False)
 
 
             # reformat sensor height and width widget
             sensor_size_widget = create_widget('VH',
-                               *self.property_widgets.get('sensor_height_px', _).findChildren(QWidget, options=direct),
-                               *self.property_widgets.get('sensor_width_px', _).findChildren(QWidget, options=direct))
-            sensor_size_widget.setEnabled(False)
+                               *self.property_widgets.get('sensor_width_px', _).findChildren(QWidget, options=direct),
+                               *self.property_widgets.get('sensor_height_px', _).findChildren(QWidget, options=direct))
+            # check if properties have setters and if not, disable widgets
+            for i, prop in enumerate(['sensor_width_px', 'sensor_height_px']):
+                attr = getattr(type(camera), prop, False)
+                if getattr(attr, 'fset', None) is None:
+                    sensor_size_widget.children()[i + 1].setEnabled(False)
 
             # reformat roi widget
             self.roi_widget = create_widget('VH',
@@ -50,15 +64,21 @@ class CameraWidget(BaseDeviceWidget):
                                 *self.property_widgets.get('height_px', _).findChildren(QWidget, options=direct),
                                 *self.property_widgets.get('height_offset_px', _).findChildren(QWidget, options=direct))
             self.roi_widget.setContentsMargins(0, 0, 0, 0)
+            # check if properties have setters and if not, disable widgets
+            for i, prop in enumerate(['width_px', 'width_offset_px', 'height_px', 'height_offset_px']):
+                attr = getattr(type(camera), prop, False)
+                if getattr(attr, 'fset', None) is None:
+                    self.roi_widget.children()[i + 1].setEnabled(False)
+
 
             central_widget = self.centralWidget()
             self.setCentralWidget(create_widget('V',
                                                 picture_buttons,
                                                 pixel_widgets,
-                                                central_widget,
                                                 timing_widgets,
                                                 sensor_size_widget,
-                                                self.roi_widget))
+                                                self.roi_widget,
+                                                central_widget))
 
     def create_live_button(self):
         """Add live button"""
